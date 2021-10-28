aa49aab2f05a3414504df0175415ec0935cb1225
duonglh711 <89597412+duonglh711@users.noreply.github.com>
2021-10-28 17:33:05 +0700

    build web app

--- comic-react/src/App.js
+++ comic-react/src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Comic web</h1>
+      <h3>Le Hoang Duong</h3>
+      <h3>Nguyen Cong Danh</h3>
     </div>
   );
 }
